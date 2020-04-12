41afba54a6ea2db7675ff25c62e44b0adad554e2
lucemia <lucemia@gmail.com>
2020-04-13 01:38:06 +0800

    fix

--- docs/annoate.py
+++ docs/annoate.py
@@ -1,19 +1,21 @@
 import json
+from collections import defaultdict
+from copy import deepcopy
 from glob import glob
 from os.path import join, realpath
 from pprint import pprint
+
 import jsonschema
-from collections import defaultdict
-from copy import deepcopy
 
 # def all_defined_types():
 #     for p in glob(f"{folder}/**/*.json"):
 #         p = "#/" + p.split("json/")[1].replace(".json", "")
 #         yield p
 
+
 class Schema(object):
     def __init__(self, folder):
-        self.folder =  folder
+        self.folder = folder
         nested_dict = lambda: defaultdict(nested_dict)
         self.cache = nested_dict()
 
@@ -22,7 +24,7 @@ class Schema(object):
             return json.load(ifile)
 
     def search(self, key, node=None):
-        print(f'{key} {node}')
+        print(f"{key} {node}")
         if node is None:
             node = self.cache
 
@@ -51,7 +53,7 @@ class Schema(object):
         assert path.startswith("#/")
 
         node = self.cache
-        for p in path.lstrip('#/').split('/'):
+        for p in path.lstrip("#/").split("/"):
             node = node[p]
 
         return node
@@ -60,7 +62,7 @@ class Schema(object):
         assert path.startswith("#/")
 
         node = self.cache
-        for p in path.lstrip('#/').split('/'):
+        for p in path.lstrip("#/").split("/"):
             if p not in node:
                 return False
             node = node[p]
@@ -73,17 +75,62 @@ class Schema(object):
 
         jsonschema.validate(data, node)
 
+    def __choose_schema(self, data, node):
+        # choose best match schema
+        if "ty" in data:
+            for s in node["oneOf"]:
+                if "$ref" in s:
+                    r = self.get(s["$ref"])
 
-    def match(self, data, node = None):
+                if r["properties"]["ty"]["const"] == data["ty"]:
+                    return s["$ref"], deepcopy(r)
+
+            raise Exception()
+
+        keys = set(data.keys())
+
+        pps = {}
+        for s in node["oneOf"]:
+            if "$ref" in s:
+                s = self.get(s["$ref"])
+
+            props = s["properties"].keys()
+            pps[s] = set(props)
+
+        pick = max(pps, key=lambda i: (pps[i] & keys) / (pps[i] | keys))
+        return pick, pps[pick]
+
+    def match(self, data, node=None):
         if node is None:
-            node = deepcopy(self.cache)
+            node = deepcopy(self.get("#/animation"))
 
-        self.validate(data, node)
+        # TODO: check it works?
+        # self.validate(data, node)
 
+        if "$ref" in node:
+            node = deepcopy(self.get(node["$ref"]))
+
+        if "oneOf" in node:
+            _, node = self.__choose_schema(data, node)
+
+        for key in node.get("properties", {}):
+            if key not in data:
+                continue
+
+            type = node["properties"][key]["type"]
+
+            if type == "object":
+                node["properties"][key] = self.match(data[key], node["properties"][key])
+            elif type == "array" and "items" in node["properties"][key]:
+                node["properties"][key] = [
+                    self.match(k, node["properties"][key]["items"]) for k in data[key]
+                ]
+
+        return node
 
 
 def analy(root):
-    schemas = Schema(folder = "../docs/json")
+    schemas = Schema(folder="../docs/json")
     queue = [root]
 
     while queue:
@@ -94,7 +141,7 @@ def analy(root):
 
         for ref in schemas.search("$ref", schemas.get(type)):
             if schemas.has(ref):
-                print('skip')
+                print("skip")
                 continue
 
             assert isinstance(ref, str)
@@ -110,10 +157,10 @@ def annotate(ifilepath, schemas):
 
     # NOTE: really?
     schemas.validate(icontent)
-
+    pprint(schemas.match(icontent))
 
 
 if __name__ == "__main__":
-    schemas = analy
+    schemas = analy("#/animation")
     pprint(schemas.cache)
     annotate("../demo/adrock/data.json", schemas)
