c4bee5d5e243d339380819e6390e252ed3a6d9fd
Lucemia Chen <lucemia@gmail.com>
2020-04-12 23:57:01 +0800

    tmp

--- docs/annoate.py
+++ docs/annoate.py
@@ -1,133 +1,119 @@
 import json
 from glob import glob
 from os.path import join, realpath
+from pprint import pprint
+import jsonschema
+from collections import defaultdict
+from copy import deepcopy
 
-folder = "./docs/json"
+# def all_defined_types():
+#     for p in glob(f"{folder}/**/*.json"):
+#         p = "#/" + p.split("json/")[1].replace(".json", "")
+#         yield p
+
+class Schema(object):
+    def __init__(self, folder):
+        self.folder =  folder
+        nested_dict = lambda: defaultdict(nested_dict)
+        self.cache = nested_dict()
+
+    def read(self, type):
+        with open(f"{self.folder}{type[1:]}.json") as ifile:
+            return json.load(ifile)
+
+    def search(self, key, node=None):
+        print(f'{key} {node}')
+        if node is None:
+            node = self.cache
+
+        if isinstance(node, dict):
+            if key in node:
+                yield node[key]
+
+            for k in node:
+                # print(k)
+                yield from self.search(key, node[k])
+
+        elif isinstance(node, list):
+            for v in node:
+                yield from self.search(key, v)
+
+    def add(self, path):
+        assert path.startswith("#/")
+
+        parent = self.get(path.rsplit("/", 1)[0])
+        parent[path.rsplit("/", 1)[1]] = self.read(path)
+
+    def get(self, path):
+        if path == "#":
+            return self.cache
+
+        assert path.startswith("#/")
+
+        node = self.cache
+        for p in path.lstrip('#/').split('/'):
+            node = node[p]
+
+        return node
+
+    def has(self, path):
+        assert path.startswith("#/")
+
+        node = self.cache
+        for p in path.lstrip('#/').split('/'):
+            if p not in node:
+                return False
+            node = node[p]
+
+        return True
+
+    def validate(self, data, node=None):
+        if node is None:
+            node = self.cache
+
+        jsonschema.validate(data, node)
 
 
-def read(type):
+    def match(self, data, node = None):
+        if node is None:
+            node = deepcopy(self.cache)
 
-    with open(f"{folder}{type[1:]}.json") as ifile:
-        return json.load(ifile)
-
-
-def search(node, key):
-    if isinstance(node, dict):
-        if key in node:
-            yield node[key]
-
-        for k in node:
-            yield from search(node[k], key)
-
-    elif isinstance(node, list):
-        for v in node:
-            yield from search(v, key)
+        self.validate(data, node)
 
 
-def all_defined_types():
-    for p in glob(f"{folder}/**/*.json"):
-        p = "#/" + p.split("json/")[1].replace(".json", "")
-        yield p
 
-
-def analysis(root):
-    schemas = {root: read(root)}
-
+def analy(root):
+    schemas = Schema(folder = "../docs/json")
     queue = [root]
 
     while queue:
         type = queue.pop()
-        schemas[type] = read(type)
+        schemas.add(type)
 
         print(f"load {type}")
 
-        for ref in search(schemas[type], "$ref"):
-            if ref in schemas:
+        for ref in schemas.search("$ref", schemas.get(type)):
+            if schemas.has(ref):
+                print('skip')
                 continue
 
             assert isinstance(ref, str)
-            assert ref.startswith("#")
+            assert ref.startswith("#/")
             queue.append(ref)
-
-    for defined_type in all_defined_types():
-        if defined_type not in schemas:
-            print(f"WARNING {defined_type} not used")
 
     return schemas
 
 
-# def guess_type(obj, accept_types, schemas):
-#     try:
-#         if len(accept_types) == 1:
-#             schema = accept_types[0]
-#         elif "ty" in obj:
-#             ty = obj["ty"]
+def annotate(ifilepath, schemas):
+    with open(ifilepath) as ifile:
+        icontent = json.load(ifile)
 
-#             for t in accept_types:
-#                 if ty == schemas[t]["properties"]["ty"].get("const") or ty == schemas[
-#                     t
-#                 ]["properties"]["ty"].get("value"):
-#                     schema = t
-#         else:
-#             schema = max(
-#                 accept_types,
-#                 key=lambda t: set(obj.keys()) & set(schemas[t]["properties"].keys()),
-#             )
-
-#         for prop in obj:
-#             if prop.startswith("$"):
-#                 continue
-
-#             if prop in known_error_props:
-#                 continue
-
-#             t = schemas[schema]["properties"][prop]
-
-#             if t["type"] == "object":
-#                 if "oneOf" in t:
-#                     guess_type(obj[prop], [k["$ref"] for k in t["oneOf"]], schemas)
-#                 else:
-#                     raise Exception()
-#             elif t["type"] == "array":
-#                 if "items" not in t:
-#                     continue
-
-#                 if "oneOf" in t["items"]:
-#                     _types = [k["$ref"] for k in t["items"]["oneOf"]]
-#                 else:
-#                     raise Exception()
-
-#                 for v in obj[prop]:
-#                     guess_type(v, _types, schemas)
-
-#         obj["$schema"] = realpath(schema.replace("#", folder)) + ".json"
-#     except Exception as e:
-#         print(f"process {obj} fail due {e}")
+    # NOTE: really?
+    schemas.validate(icontent)
 
 
-# def update_schema(node):
-#     if not "$shema" in node:
-#         return
 
-#     schema = schemas[node["$schema"]]
-
-#     for p in node:
-#         if isinstance(node[p], list):
-#             sub_schema = [update_schema(k) for k in node[p]]
-#         elif isinstance(node[p], dict):
-#             sub_schema = {k: update_schema(node[p][k]) for k in node[p]}
-
-
-# def annotate(p):
-#     with open(p) as ifile:
-#         icontent = json.load(ifile)
-
-#         guess_type(icontent, ["#/animation"], schemas)
-
-#     # with open(p + ".schema.json", "w") as ofile:
-
-
-analysis("#/animation")
-
-
-# annotate("./demo/adrock/data.json")
+if __name__ == "__main__":
+    schemas = analy
+    pprint(schemas.cache)
+    annotate("../demo/adrock/data.json", schemas)
